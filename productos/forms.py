8d9fec15174b7fbd349e0ec750efbbd8587c4739
sddrouet@espe.edu.ec <sddrouet@espe.edu.ec>
2024-06-01 23:31:21 -0500

    Agregando funciones elimianr y editar, separando css de base.html

--- productos/forms.py
+++ productos/forms.py
@@ -11,3 +11,20 @@ class ProductoForm(forms.ModelForm):
             'precio': forms.NumberInput(attrs={'class': 'form-control'}),
             'cantidad': forms.NumberInput(attrs={'class': 'form-control'}),
         }
+
+# creating a form
+class EditarForm(forms.ModelForm):
+ 
+    # create meta class
+    class Meta:
+        # specify model to be used
+        model = Producto
+ 
+        # specify fields to be used
+        fields = ['nombre', 'precio', 'cantidad']  
+
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'precio': forms.NumberInput(attrs={'class': 'form-control'}),
+            'cantidad': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
